3ce17b43760bbd5fe3b45005ffa46d3f50e72a20
UnorthoCode <17720692+UnorthoCode@users.noreply.github.com>
2018-01-15 19:35:46 -0800

    Add files via upload

---
+++ Heavy Weapon/cGame.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <Windows.h>
+
+class cGame
+{
+public:
+	DWORD dwLives;
+	DWORD dwNukes;
+	DWORD dwShield;
+	DWORD dwSpeedIncreaseUpgrade; 
+	DWORD dwGunPowerUpgrade;
+	DWORD dwRapidFireUpgrade;
+	DWORD dwSpreadShotUpgrade;
+	DWORD dwBallShieldUpgrade;
+	DWORD dwHomingMissileUpgrade;
+	DWORD dwLaserUpgrade;
+	DWORD dwMissileUpgrade;
+	DWORD dwBurstUpgrade;
+	DWORD dwLightningUpgrade;
+
+};
